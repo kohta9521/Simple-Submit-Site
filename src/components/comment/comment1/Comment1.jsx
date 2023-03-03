3865108a6f8d4c8fcebcbc7c04b3747be2504fa2
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-03-03 13:26:58 +0900

    add comment1 box html

--- src/components/comment/comment1/Comment1.jsx
+++ src/components/comment/comment1/Comment1.jsx
@@ -4,6 +4,17 @@ import "./Comment1.css";
 
 export default function Comment1() {
     return (
-        <div>Comment1</div>
+    <>
+    <div className="comment">
+        <div className="commentContainer">
+            <img className='commentCharImg' src="./assets/icon-char.png" alt="らこたん画像" />
+            <div className="commentBox">
+                はじめまして！<span class="text-pink">らこたん</span>です。<br />
+                あなたの転職のお手伝いをさせていただきます。<br />
+                はじめに、<span class="text-blue">お持ちの資格</span>をご選択ください。
+            </div>
+        </div>
+    </div>
+    </>
     );
-}
+}   
